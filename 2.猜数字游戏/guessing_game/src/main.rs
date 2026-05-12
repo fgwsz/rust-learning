fb4c6c129edb22ad59f5d78da59ca3ede3695f84
fgwsz <3191000458@qq.com>
2026-05-13 01:41:13 +0800

    优化guessing_game,减少内存分配

--- 2.猜数字游戏/guessing_game/src/main.rs
+++ 2.猜数字游戏/guessing_game/src/main.rs
@@ -7,13 +7,15 @@ fn main() {
 
     let secter_number=rand::thread_rng().gen_range(1..101);
 
+    let mut guess=String::new();//放到loop循环外,规避堆内存频繁分配
+
     loop{
         println!("Please input your guess.");
 
-        let mut guess=String::new();
+        guess.clear(); //清空
 
         io::stdin()
-            .read_line(&mut guess)
+            .read_line(&mut guess) //注意:read_line是追加内容到字符串,而不是覆盖
             .expect("Failed to read line");
 
         let guess:u32=match guess.trim().parse(){
